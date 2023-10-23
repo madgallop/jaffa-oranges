adeaf8224db8e488caa0b8b76fa8f171d3c7ac9c
madgallop <mgallop@middlebury.edu>
2023-10-23 11:31:43 -0400

    edit text and pitch
    take out text for now

--- docs/config.js
+++ docs/config.js
@@ -7,12 +7,12 @@ var config = {
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
-    theme: 'dark',
+    theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: 'Jaffa Oranges',
     subtitle: "Agriculture, dispossession, and the shifting narrative of the orange groves of Palestine's ancient port city",
-    byline: 'By Madeleine Gallop',
+    byline: 'Madeleine Gallop',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -21,7 +21,7 @@ var config = {
             hidden: false,
             title: 'Display Title',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            //description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [34.75937, 32.06252],
                 zoom: 12.44,
@@ -55,7 +55,7 @@ var config = {
             location: {
                 center: [34.75937, 32.06252],
                 zoom: 12.44,
-                pitch: 60,
+                pitch: 0.00,
                 bearing: 0.00,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
@@ -76,7 +76,7 @@ var config = {
             hidden: false,
             title: 'Third Title',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            //description: 'Copy these sections to add to your story.',
             location: {
                 center: [34.75937, 32.06252],
                 zoom: 12.44,
@@ -95,7 +95,7 @@ var config = {
             hidden: false,
             title: 'Third Title',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            //description: 'Copy these sections to add to your story.',
             location: {
                 center: [34.75937, 32.06252],
                 zoom: 12.44,
