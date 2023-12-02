9f0a6b4311be6751f01feec1a50bf1f49984cb01
madgallop <mgallop@middlebury.edu>
2023-12-02 12:10:41 -0500

    drop inset map and add a zoom chapter at beginning
    slayful

--- docs/config.js
+++ docs/config.js
@@ -16,6 +16,47 @@ var config = {
     // footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
+            id: 'inset-intro',
+            alignment: 'left',
+            hidden: false,
+            // title: "",
+            //image: './path/to/image/source.png',
+            // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [34.72776, 32.05656],
+                zoom: 5,
+                pitch: 0.00,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'jafffa-orange-groves',
+                    //filter: 
+                    opacity: [
+                        "match",
+                        ["get", "year"],
+                        ["1918"],
+                        0,
+                        ["1943"],
+                        0,
+                        ["1958"],
+                        0,
+                        0
+                    ],
+                    //duration: 5000,
+                }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'jafffa-orange-groves',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
